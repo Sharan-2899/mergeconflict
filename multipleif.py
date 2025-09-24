368f1588a21cb5fdcd4e6508c585e137c3278b32
Sharan-2899 <totti031999@gmail.com>
2025-09-24 15:33:18 +0530

    new file added

---
+++ multipleif.py
@@ -0,0 +1,10 @@
+age=int(input("enter ur age: "))
+
+if age>=18:
+    print("you can ride roller coster now!")
+if age>=15:
+    print("you are allowed to ride ship now") 
+if age>=12:
+    print("you are allowed to play arcade games")     
+else:
+    print("your'e under age")      
