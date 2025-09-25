064ffc2424f97668f1c4361b5ec86ba7df870fc5
Sharan-2899 <totti031999@gmail.com>
2025-09-25 16:54:02 +0530

    updated new changes

--- for_while.py
+++ for_while.py
@@ -1,5 +1,5 @@
 # for loop is a control flow statement used to iterate over a sequence of time until it meets the condition...we can iterate sting , list , dict , tuple etcname
-name ="           "
+name ="      "
 
 reversed_string="*"
 
