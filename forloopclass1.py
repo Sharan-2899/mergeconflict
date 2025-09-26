4a23af2434ec8b7fa1d2f329b466530c20fe7321
Sharan-2899 <totti031999@gmail.com>
2025-09-26 15:32:03 +0530

    made changes

--- forloopclass1.py
+++ forloopclass1.py
@@ -1,6 +1,38 @@
+# task1
+
 for i in range(len("hello")):
     for j in range(len("hi")):
         print(i,j)
 
+# task2
+
+x=['1','2','hi','hello','12','ddhe','ss','sss','w22','ss']
+y=0
+
+for i in x:
+    y=y+1
+    if y%2==0:
+        print(y)
 
 
+# task2
+
+x={'1','2','hi','hello','12','ddhe','ss','sss','w22','ss'}
+y=0
+
+for i in x:
+    y=y+1
+    if y%2==0:
+        print(y)
+
+
+# task3
+
+
+x={"hell0","hi","python","html"}
+
+for i in x:
+    print(i)
+
+
+# enumerate    
