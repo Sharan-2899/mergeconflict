54116a1f85768592b3159e6d015ae906df96eaa1
Sharan-2899 <totti031999@gmail.com>
2025-10-15 15:55:26 +0530

    new file created

--- 5_function_types.py
+++ 5_function_types.py
@@ -23,3 +23,39 @@ def student(name, age):
 
 student(name="Sharan", age=20)
 student("Sharan", age=20)   # keyword args followed positional arguments
+
+# default arguments
+'''If you don’t provide a value for a parameter, Python will use its default value.'''
+def greet(name, message="Good Morning"):
+    print(f"Hello {name}, {message}!")
+
+greet("Sharan")
+greet("Riya", "Welcome!")
+
+# Variable-length Arguments – *args
+'''Used when you don’t know how many positional arguments you’ll get.'''
+def add(*numbers):
+    print(sum(numbers))
+
+add(1, 2)
+add(3, 4, 5, 6)
+
+#Keyword Variable-length Arguments – **kwargs
+'''Used when you don’t know how many keyword arguments you’ll get.'''
+def info(**details):
+    print(details)
+
+info(name="Sharan", age=20, city="Bangalore")
+
+# Mixing Argument Types
+'''You can mix them, but order must be:  Positional → *args → Default → **kwargs  '''
+
+def profile(name, *hobbies, age=18, **info):
+    print(name, hobbies, age, info)
+
+profile("Sharan", "Cricket", "Coding", age=20, city="Bangalore", college="MIT")
+
+
+
+
+
